d86383649c72835241a9f4c806033f239f2726d8
Martin Bonnin <martin@mbonnin.net>
2025-04-25 10:33:27 +0200

    remove SNAPSHOTs usage

--- tests/kmp/settings.gradle.kts
+++ tests/kmp/settings.gradle.kts
@@ -2,7 +2,6 @@ pluginManagement {
     listOf(repositories, dependencyResolutionManagement.repositories).forEach {
         it.apply {
             mavenCentral()
-            maven("https://storage.googleapis.com/gradleup/m2")
         }
     }
 }
