5c271309dd304df6bc94834cc2f2be6a9ff92f99
Martin Bonnin <martin@mbonnin.net>
2025-07-05 15:28:14 +0200

    hide more symbols

--- nmcp/src/main/kotlin/nmcp/plugins.kt
+++ nmcp/src/main/kotlin/nmcp/plugins.kt
@@ -9,12 +9,12 @@ import org.gradle.api.Project
 import org.gradle.api.initialization.Settings
 
 @GPlugin(id = "com.gradleup.nmcp")
-fun nmcp(project: Project) {
+internal fun nmcp(project: Project) {
     project.extensions.create(NmcpExtension::class.java, "nmcp", DefaultNmcpExtension::class.java, project)
 }
 
 @GPlugin(id = "com.gradleup.nmcp.aggregation")
-fun nmcpAggregation(project: Project) {
+internal fun nmcpAggregation(project: Project) {
     project.extensions.create(NmcpAggregationExtension::class.java, "nmcpAggregation", DefaultNmcpAggregationExtension::class.java, project)
 }
 
