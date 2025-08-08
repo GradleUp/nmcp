c01e67a44b68f38ff604334504cad65a7d5a019a
Martin Bonnin <martin@mbonnin.net>
2025-08-08 21:36:41 +0200

    Hide nmcpClient

--- nmcp/src/main/kotlin/nmcp/transport/okhttp.kt
+++ nmcp/src/main/kotlin/nmcp/transport/okhttp.kt
@@ -3,7 +3,7 @@ package nmcp.transport
 import java.time.Duration
 import okhttp3.OkHttpClient
 
-val nmcpClient = OkHttpClient.Builder()
+internal val nmcpClient = OkHttpClient.Builder()
     .connectTimeout(Duration.ofSeconds(30))
     .writeTimeout(Duration.ofSeconds(30))
     .readTimeout(Duration.ofSeconds(60))
