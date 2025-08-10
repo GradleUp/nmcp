4d4925b9c73386770753b486d3884cbc785f0bd9
Martin Bonnin <martin@mbonnin.net>
2025-08-10 15:56:20 +0200

    bump versions (#174)

--- scripts/update-repo.main.kts
+++ scripts/update-repo.main.kts
@@ -3,7 +3,7 @@
 @file:Repository("https://repo.maven.apache.org/maven2/")
 @file:Repository("https://storage.googleapis.com/gradleup/m2")
 @file:Repository("https://jitpack.io")
-@file:DependsOn("com.gradleup.librarian:librarian-cli:0.0.11-SNAPSHOT-e8ab4653f68118d36889519f430f1fcd118745e3")
+@file:DependsOn("com.gradleup.librarian:librarian-cli:0.0.11-SNAPSHOT-f1a6a7856f6bfcd2c9c28e2cff9d316e5f191142")
 
 import com.gradleup.librarian.cli.updateRepo
 
