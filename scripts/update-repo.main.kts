20ae01e0512e860f152655a89b41d530c790b16d
Martin Bonnin <martin@mbonnin.net>
2025-08-20 10:15:51 +0200

    version is now 1.1.0-SNAPSHOT

--- scripts/update-repo.main.kts
+++ scripts/update-repo.main.kts
@@ -3,13 +3,13 @@
 @file:Repository("https://repo.maven.apache.org/maven2/")
 @file:Repository("https://storage.googleapis.com/gradleup/m2")
 @file:Repository("https://jitpack.io")
-@file:DependsOn("com.gradleup.librarian:librarian-cli:0.0.11-SNAPSHOT-f1a6a7856f6bfcd2c9c28e2cff9d316e5f191142")
+@file:DependsOn("com.gradleup.librarian:librarian-cli:0.1.1-SNAPSHOT-44a36034d83f8ba9c9177653f29df3dea933fc3f")
 
 import com.gradleup.librarian.cli.updateRepo
 
 updateRepo(args) {
     file("README.md") {
-        replacePluginId("com.gradleup.nmcp.aggregation")
-        replacePluginId("com.gradleup.nmcp")
+        replacePluginVersion("com.gradleup.nmcp.aggregation")
+        replacePluginVersion("com.gradleup.nmcp")
     }
 }
