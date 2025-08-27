cd9e387c84b718fd40759d90e84dff942dc5658a
Martin Bonnin <martin@mbonnin.net>
2025-08-27 17:13:50 +0200

    Use the snapshot version of the gratatouille plugin (#187)
    * Use the snapshot version of gratatouille
    
    * fix getting the KSP processor

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,16 +5,16 @@ pluginManagement {
     ).forEach {
         it.apply {
             mavenCentral()
+            maven("https://storage.googleapis.com/gradleup/m2") {
+                content {
+                    // this is only used at build time and is safe to fetch as a snapshot
+                    includeModule("com.gradleup.gratatouille", "gratatouille-processor")
+                }
+            }
         }
     }
     repositories {
-        maven("https://storage.googleapis.com/gradleup/m2") {
-            content {
-                includeGroup("com.gradleup.librarian")
-                includeGroup("com.gradleup.nmcp")
-                includeGroup("com.gradleup.compat.patrouille")
-            }
-        }
+        maven("https://storage.googleapis.com/gradleup/m2")
     }
 }
 
