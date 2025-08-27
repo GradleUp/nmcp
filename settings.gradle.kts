0b0de8c1c2a984ad476b39661b2507de40e190ad
Martin Bonnin <martin@mbonnin.net>
2025-08-27 17:09:46 +0200

    fix getting the KSP processor

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,12 @@ pluginManagement {
     ).forEach {
         it.apply {
             mavenCentral()
+            maven("https://storage.googleapis.com/gradleup/m2") {
+                content {
+                    // this is only used at build time and is safe to fetch as a snapshot
+                    includeModule("com.gradleup.gratatouille", "gratatouille-processor")
+                }
+            }
         }
     }
     repositories {
