9f970a4e9cb7dddcfd9e771be3b016005337c167
Martin Bonnin <martin@mbonnin.net>
2025-09-15 16:28:35 +0200

    Bump bootstrapped version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
             mavenCentral()
             maven("https://storage.googleapis.com/gradleup/m2") {
                 content {
-                    // this is only used at build time and is safe to fetch as a snapshot
+                    // gratatouille-processor is only used at build time and is safe to fetch as a snapshot
                     includeModule("com.gradleup.gratatouille", "gratatouille-processor")
                 }
             }
