f9ad7bf45e0dbdf89ef56ffa3c24ae6d72919df2
Martin Bonnin <martin@mbonnin.net>
2025-12-15 13:03:38 +0100

    Expand the zip files when passing it to `allFiles` (#218)

--- nmcp/src/main/kotlin/nmcp/internal/DefaultNmcpAggregationExtension.kt
+++ nmcp/src/main/kotlin/nmcp/internal/DefaultNmcpAggregationExtension.kt
@@ -4,6 +4,7 @@ import nmcp.CentralPortalOptions
 import nmcp.NmcpAggregationExtension
 import org.gradle.api.Action
 import org.gradle.api.Project
+import org.gradle.api.file.FileCollection
 
 internal abstract class DefaultNmcpAggregationExtension(private val project: Project) : NmcpAggregationExtension {
     private val spec = project.objects.newInstance(CentralPortalOptions::class.java)
@@ -15,7 +16,7 @@ internal abstract class DefaultNmcpAggregationExtension(private val project: Pro
         it.configureAttributes(project)
     }
 
-    override val allFiles = consumerConfiguration.incoming.artifactView { it.lenient(true) }.files
+    override val allFiles: FileCollection = consumerConfiguration.incoming.artifactView { it.lenient(true) }.files.asFileTree
 
     init {
 
