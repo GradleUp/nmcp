b34a824fbce79a04abc7b9033337345e1b590616
Martin Bonnin <martin@mbonnin.net>
2026-08-31 12:11:24 +0200

    Version is now 1.6.3-SNAPSHOT

--- nmcp/testProjects/duplicate-name/build.gradle.kts
+++ nmcp/testProjects/duplicate-name/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
   id("org.jetbrains.kotlin.jvm").version("2.3.0").apply(false)
-  id("com.gradleup.nmcp.aggregation").version("1.6.2-SNAPSHOT")
+  id("com.gradleup.nmcp.aggregation").version("1.6.3-SNAPSHOT")
 }
 
 group = "com.example"
