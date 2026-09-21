f32cd80ef232df015f994fb98102b1176bf50326
Martin Bonnin <martin@mbonnin.net>
2026-09-21 11:54:19 +0200

    Update error message

--- nmcp/src/main/kotlin/nmcp/internal/DefaultNmcpAggregationExtension.kt
+++ nmcp/src/main/kotlin/nmcp/internal/DefaultNmcpAggregationExtension.kt
@@ -67,10 +67,10 @@ internal abstract class DefaultNmcpAggregationExtension(private val project: Pro
                 project.allprojects {
                     check(!allNames.contains(it.name.lowercase())) {
                         """
-                         Nmcp: some projects have the same name: '${'$'}{it.name}'. This creates issues when resolving the aggregation.
+                         Nmcp: some projects have the same name: '${'$'}{it.name}'. If your projects also have the same groupId, this can create issues when resolving the aggregation.
                          You can usually resolve this error by renaming your projects (including possibly your root project).
                          Or you can disable this check by calling `allowDuplicateProjectNames.set(true)`.
-                         See https://github.com/gradle/gradle/issues/36167 for more details.
+                         See https://github.com/gradle/gradle/issues/847 for more details.
                         """.trimIndent()
                     }
                     allNames.add(it.name.lowercase())
