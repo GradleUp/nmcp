a1fce3fa9fbd8145cc49a89eb8cddde4f50a5d47
Adam <897017+aSemy@users.noreply.github.com>
2026-09-21 11:55:00 +0200

    Fix string interpolation in project name check (#282)

--- nmcp/src/main/kotlin/nmcp/internal/DefaultNmcpAggregationExtension.kt
+++ nmcp/src/main/kotlin/nmcp/internal/DefaultNmcpAggregationExtension.kt
@@ -67,7 +67,7 @@ internal abstract class DefaultNmcpAggregationExtension(private val project: Pro
                 project.allprojects {
                     check(!allNames.contains(it.name.lowercase())) {
                         """
-                         Nmcp: some projects have the same name: '${'$'}{it.name}'. This creates issues when resolving the aggregation.
+                         Nmcp: some projects have the same name: '${it.name}'. This creates issues when resolving the aggregation.
                          You can usually resolve this error by renaming your projects (including possibly your root project).
                          Or you can disable this check by calling `allowDuplicateProjectNames.set(true)`.
                          See https://github.com/gradle/gradle/issues/36167 for more details.
